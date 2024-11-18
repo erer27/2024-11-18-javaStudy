4cee5965ea6452d9afff4adea95292cf554e8b30
sist-108 <sist-108@DESKTOP-KB9HU04>
2024-11-18 14:22:17 +0900

    커밋3

--- FirstProject/src/첫번째자바.java
+++ FirstProject/src/첫번째자바.java
@@ -80,7 +80,7 @@ public class 첫번째자바 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java");
-		System.out.println("오승준1111");
+		System.out.println("오승준2222");
 		{
 			
 		}
