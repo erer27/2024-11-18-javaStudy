f478cc9b02903d0a4d369602659a514ab6227ecd
sist-108 <sist-108@DESKTOP-KB9HU04>
2024-12-09 17:49:10 +0900

    2024-12-09

---
+++ 객체지향프로그램_1/src/객체지향프로그램_1/클래스_문제.java
@@ -0,0 +1,104 @@
+package 객체지향프로그램_1;
+
+import java.util.Date;
+
+class Music{
+	String picture;
+	String title;
+	String artist;
+	String album;
+	int like;
+	int change;
+}
+
+class Recipee{
+	String picture;
+	String title;
+	String chef;
+	int star;
+	int view;
+}
+class Restaurant{
+	String picture;
+	String name;
+	String category;
+	String phone;
+	String address;
+	int star;
+	int patient;
+	String[] theme;
+}
+
+class Detail{
+	String time;
+	String rankHistory;
+	String restDay;
+	String chairAndroom;
+	boolean alcohol;
+	String smoke;
+	String reserveInfo;
+	String restRoom;
+	String parking;
+	String facility;
+	String intro;
+}
+
+class SignUp
+{
+	String id;
+	String pwd;
+	String pwdCheck;
+	String name;
+	String sex;
+	Date BirthDay;
+	String eMail;
+	String zip;
+	String address;
+	String detailAddress;
+	String phone;
+	String intro;
+}
+
+class Restaurant2{
+	String address;
+	String phone;
+	String food;
+	String time;
+	String parking;
+	String menu;
+	
+}
+
+class Movie{
+	String poster;
+	String title;
+	String originalTitle;
+	String open;
+	float critic;
+	String genre;
+	int accum;
+	String nation;
+	String rank;
+	String age;
+	int runningTime;
+}
+
+class Goods{
+	String poster;
+	String title;
+	String description;
+	int sale;
+	int price;
+	int firstPrice;
+	int star;
+	String deliver;
+	int point;
+}
+public class 클래스_문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
