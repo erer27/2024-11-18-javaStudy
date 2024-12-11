d8cc9a3ee9c0cfd235a4f57c6d718a9a6879e1fe
sist-108 <sist-108@DESKTOP-KB9HU04>
2024-12-11 17:48:30 +0900

    2024-12-11

---
+++ Chapter)5_객체지향프로그램_5/src/com/sist/main/SawonUserMain.java
@@ -0,0 +1,14 @@
+package com.sist.main;
+import com.sist.vo.*; // 라이브러리, 사용자 정의 => 패키지가 다른 경우 import를 사용해서
+public class SawonUserMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수=> 읽기/쓰기 => 기능을 만든다
+		// 초기화 => 생성자 => 
+		Sawon hong=new Sawon(1,"홍길동","개발부","대리","2021-10-10",3600);
+		
+		
+	}
+
+}
---
+++ Chapter05_객체지향프로그램_6/src/com/sist/main/문제.java
@@ -0,0 +1,23 @@
+package com.sist.main;
+
+class Member{
+	private String name;
+	private String id;
+	private String password;
+	private int age;
+	
+	public Member(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
+
+
+public class 문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
