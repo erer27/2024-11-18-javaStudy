d4119a265723521c084b7bc6745b0e92b5fe628e
sist-108 <sist-108@DESKTOP-KB9HU04>
2024-12-16 12:36:17 +0900

    2024-12-16

---
+++ Chapter07_클래스 종류/src/com/sist/oracle/Oracleeee.java
@@ -0,0 +1,31 @@
+package com.sist.oracle;
+import java.sql.*;
+public class Oracleeee {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		// 드라이버 설정
+		Class.forName("oracle.jdbc.dirver.OracleDriver");
+		// ojdbc8.jar
+		// 오라클 연결
+		String url="jdbc:oracle:thin:@localhost:1521:XE";
+		Connection conn=DriverManager.getConnection(url,"hr","hapyy");
+		// conn hr/happy
+		String sql="SELECT * FROM emp";
+		// 오라클 => SQL문장 전송
+		// SQL
+		PreparedStatement ps=conn.prepareStatement(sql);
+		// 실행결과
+		ResultSet rs=ps.executeQuery();
+		// rs에 실행된 데이터가 들어가 있다
+		while(rs.next()) // 데이터가 시작 ~ 끝까지 읽어 온다
+		{
+			System.out.println(
+					rs.getInt(1)+" "
+					+rs.getString(2)+""
+					+rs.getString(3)
+			);
+		}
+	}
+
+}
